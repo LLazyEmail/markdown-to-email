668b822e5fac3ae60d887af092180fd28ccd1038
hirdbluebird <bluebirdyourdestiny@gmail.com>
2020-10-07 19:31:49 +0300

    npm command added, concat opration written

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const path = require('path')
+const fs = require('fs')
+let header,
+  footer,
+  socials,
+  promo,
+  section;
+
+
+header = fs.readFileSync(`./layouts/header.html`, 'utf8', function(err, data) {
+  if (err) throw new Error(`file not found`)
+   // data;
+})
+
+socials = fs.readFileSync(`./layouts/socials.html`, 'utf8', function(err, data) {
+  if (err) throw new Error(`file not found`)
+   // data;
+})
+
+footer = fs.readFileSync(`./layouts/footer.html`, 'utf8', function(err, data) {
+  if (err) throw new Error(`file not found`)
+   // data;
+})
+
+promo = fs.readFileSync(`./layouts/body/promo.html`, 'utf8', function(err, data) {
+  if (err) throw new Error(`file not found`)
+  // data;
+  // console.log(promo);
+})
+
+section = fs.readFileSync(`./layouts/body/section.html`, 'utf8', function(err, data) {
+  if (err) throw new Error(`file not found`)
+   // = data;
+})
+
+const newFile = String.prototype.concat(header, promo, section, promo, socials, footer);
+
+fs.writeFile('./generated/newEmail.html', newFile, 'utf8', function(err) {
+  if (err) throw new Error('file not written')
+  console.log(newFile);
+  console.log('file successfully written')
+})
