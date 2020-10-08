b43a0623887f78d1503e09f3d830bc0cead7c640
hirdbluebird <bluebirdyourdestiny@gmail.com>
2020-10-08 19:04:56 +0300

    6 hours: parser for major tags created

--- index.js
+++ index.js
@@ -27,10 +27,85 @@ promo = fs.readFileSync(`./layouts/body/promo.html`, 'utf8', function(err, data)
   // console.log(promo);
 })
 
-section = fs.readFileSync(`./layouts/body/section.html`, 'utf8', function(err, data) {
-  if (err) throw new Error(`file not found`)
-   // = data;
-})
+// section = fs.readFileSync(`./layouts/body/section.html`, 'utf8', function(err, data) {
+//   if (err) throw new Error(`file not found`)
+//    // = data;
+// })
+
+
+let emailBody;
+const source = fs.readFileSync('./source/source.html', 'utf8')
+
+function parseSource(source) {
+  let thisSource = source;
+
+  function replaceTags() {
+
+    if (thisSource.indexOf('<') !== -1) {
+      const indexOfTag = thisSource.indexOf('<');
+      const tag = thisSource.slice(indexOfTag, indexOfTag + 3);
+
+      switch(tag) {
+        case '<h1':
+          convertTag('<h1>', '</h1>', './layouts/typography/mainTitle.html')
+          break;
+        case '<p>':
+          convertTag('<p>', '</p>', './layouts/typography/paragraph.html')
+          break;
+        case '<h2':
+          convertTag('<h2>', '</h2>', './layouts/typography/subtitle.html')
+        case '<im':
+          convertTag('<img', '/>', './layouts/typography/image.html')
+        default:
+          console.log('no tag detected')
+          break;
+      }
+
+      replaceTags()
+    }
+  }
+  replaceTags()
+
+  // console.log('Document converted successfuly')
+  // console.log(emailBody)
+
+  function convertTag(openingTag, closingTag, templatePath) {
+    const start = thisSource.indexOf(openingTag) + openingTag.length
+    const end = thisSource.indexOf(closingTag)
+    const text = thisSource.slice(start, end)
+
+    if (openingTag === '<img') {
+      const src = /src=\"(.*?)\"/g.exec(text)
+      const href = /href=\"(.*?)\"/g.exec(text)
+
+      if (src && href) {
+        console.log(src[1], href[1]);
+
+        emailBody += fs.readFileSync(templatePath, 'utf8')
+                        .replace('{src}', src)
+                        .replace('{href}', href)
+
+        thisSource = thisSource.slice(end + closingTag.length);
+      }
+      return;
+    }
+
+    emailBody += fs.readFileSync(templatePath, 'utf8')
+                    .replace('{content}', text)
+
+    thisSource = thisSource.slice(end + closingTag.length);
+    // console.log(thisSource);
+  }
+  // console.log(thisSource)
+
+  section = fs.readFileSync('./layouts/body/section.html', 'utf8')
+              .replace('{content}', emailBody)
+
+}
+parseSource(source)
+
+
+
 
 const newFile = String.prototype.concat(header, promo, section, promo, socials, footer);
 
