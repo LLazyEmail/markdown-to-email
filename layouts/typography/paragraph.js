87981b655beeee8b0a88e19a1f24928b6dd84fc4
Vadim <vadim.putrov@gmail.com>
2020-10-24 18:47:30 +0300

    adding wrappers to just a guess

--- layouts/typography/paragraph.js
+++ layouts/typography/paragraph.js
@@ -1,10 +1,1 @@
-module.exports = `<div dir="ltr" style="text-align: justify;">
-<span style="font-size:16px">
-  <span style="font-family:trebuchet ms,lucida grande,lucida sans unicode,lucida sans,tahoma,sans-serif">
-    {content}
-  </span>
-</span>
-</div>
-
--------------pppppp--------------------
-`;
+module.exports = `<div dir="ltr" style="text-align: justify;"><span style="font-size:16px"><span style="font-family:trebuchet ms,lucida grande,lucida sans unicode,lucida sans,tahoma,sans-serif">{content}</span></span></div>`;
