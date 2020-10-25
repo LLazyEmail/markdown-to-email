97e8822fdb0a57a2e2f4239051a8c0e682b28e9b
Vadim <vadim.putrov@gmail.com>
2020-10-25 11:18:27 +0200

    move functions out

---
+++ src/just-a-guess-functions.js
@@ -0,0 +1,66 @@
+const readFile = require('./parseSource');
+
+function link(text, title, href) {
+  return readFile('typography/link').replace("{content}", title.trim()).replace("{href}", href.trim())
+}
+'<a href=\'$2\'>$1</a>'
+
+function para(text, line) {
+  debugger;
+  var trimmed = line.trim();
+  if (/^<\/?(ul|ol|li|h|p|bl)/i.test(trimmed)) {
+    return '\n' + line + '\n';
+  }
+  // return '\n<p>' + trimmed + '</p>\n';
+  return readFile('typography/paragraph').replace('{content}', trimmed);
+}
+
+function ulList(text, item) {
+  return '\n<ul>\n\t<li>' + item.trim() + '</li>\n</ul>';
+}
+
+function olList(text, item) {
+  return '\n<ol>\n\t<li>' + item.trim() + '</li>\n</ol>';
+}
+
+function blockquote(text, tmp, item) {
+  return '\n<blockquote>' + item.trim() + '</blockquote>';
+}
+function image(text, alt, src) {
+  const parsedSrc = src.trim().match(/^(?<src>(.*?))\s?((?<quote>")(?<tooltip>.*?)\k<quote>)?$/) || [];
+  if (parsedSrc && parsedSrc.groups && parsedSrc.groups.src) {
+    return readFile('typography/image')
+      .replace('{src}', parsedSrc.groups.src)
+      .replace('{altText}', alt)
+  }
+  return readFile('typography/image')
+    .replace('{src}', '')
+    .replace('{altText}', alt)
+}
+
+function header(text, chars, content) {
+  var level = chars.length;
+  // console.log(content.trim());
+  // console.log(readFile('typography/mainTitle').replace('{content}', content.trim()));
+  switch (level) {
+    case 1:
+      return readFile('typography/mainTitle').replace('{content}', content.trim());
+      break;
+    case 2:
+      return readFile('typography/subtitle').replace('{content}', content.trim());
+    default:
+      break;
+  }
+
+  // return '<h' + level + '>' + content.trim() + '</h' + level + '>';
+}
+
+// function subtitle (text, chars, content) {
+//   var level = chars.length;
+//   return readFile('typography/subtitle').replace('{content}', content.trim());
+//   // return '<h' + level + '>' + content.trim() + '</h' + level + '>';
+// }
+
+module.exports = {
+  link, para, ulList, olList, blockquote, image, header
+}
--- src/just-a-guess.js
+++ src/just-a-guess.js
@@ -1,8 +1,7 @@
 // from https://gist.github.com/renehamburger/12f14a9bd9297394e5bd
 const fs = require('fs');
-const readFile = require('./parseSource');
-
-const {write} = require('./utils');
+const { header, image, link, ulList, olList, blockquote, para } = require('./just-a-guess-functions')
+const { write } = require('./utils');
 
 'use strict';
 
@@ -33,7 +32,7 @@ function Slimdown() {
   this.rules = [
     { regex: /(#+)(.*)/g, replacement: header },                                         // headers
     { regex: /!\[([^\[]+)\]\(([^\)]+)\)/g, replacement: image }, // image
-    { regex: /\[([^\[]+)\]\(([^\)]+)\)/g, replacement:  link},        // hyperlink
+    { regex: /\[([^\[]+)\]\(([^\)]+)\)/g, replacement: link },        // hyperlink
     { regex: /(\*\*|__)(.*?)\1/g, replacement: '<strong>$2</strong>' },                  // bold
     // { regex: /(\*|_)(.*?)\1/g, replacement: '<em>$2</em>' },    @FIX insirting it into alt                         // emphasis
     { regex: /\~\~(.*?)\~\~/g, replacement: '<del>$1</del>' },                           // del
@@ -64,66 +63,7 @@ function Slimdown() {
     });
     return text.trim();
   };
-  function link(text, title, href){
-    return readFile('typography/link').replace("{content}", title.trim()).replace("{href}", href.trim())
-  }
-  '<a href=\'$2\'>$1</a>'
-  
-  function para(text, line) {
-    debugger;
-    var trimmed = line.trim();
-    if (/^<\/?(ul|ol|li|h|p|bl)/i.test(trimmed)) {
-      return '\n' + line + '\n';
-    }
-    // return '\n<p>' + trimmed + '</p>\n';
-    return readFile('typography/paragraph').replace('{content}', trimmed);
-  }
 
-  function ulList(text, item) {
-    return '\n<ul>\n\t<li>' + item.trim() + '</li>\n</ul>';
-  }
-
-  function olList(text, item) {
-    return '\n<ol>\n\t<li>' + item.trim() + '</li>\n</ol>';
-  }
-
-  function blockquote(text, tmp, item) {
-    return '\n<blockquote>' + item.trim() + '</blockquote>';
-  }
-  function image(text, alt, src) {
-    const parsedSrc = src.trim().match(/^(?<src>(.*?))\s?((?<quote>")(?<tooltip>.*?)\k<quote>)?$/) || [];
-    if (parsedSrc && parsedSrc.groups && parsedSrc.groups.src){
-      return readFile('typography/image')
-      .replace('{src}', parsedSrc.groups.src)
-      .replace('{altText}', alt)
-    }
-    return readFile('typography/image')
-    .replace('{src}', '#')
-    .replace('{altText}', alt)
-  }
-
-  function header(text, chars, content) {
-    var level = chars.length;
-    // console.log(content.trim());
-    // console.log(readFile('typography/mainTitle').replace('{content}', content.trim()));
-    switch (level) {
-      case 1:
-        return readFile('typography/mainTitle').replace('{content}', content.trim());
-        break;
-      case 2:
-        return readFile('typography/subtitle').replace('{content}', content.trim());
-      default:
-        break;
-    }
-
-    // return '<h' + level + '>' + content.trim() + '</h' + level + '>';
-  }
-
-  // function subtitle (text, chars, content) {
-  //   var level = chars.length;
-  //   return readFile('typography/subtitle').replace('{content}', content.trim());
-  //   // return '<h' + level + '>' + content.trim() + '</h' + level + '>';
-  // }
 }
 
 var sd = new Slimdown();
