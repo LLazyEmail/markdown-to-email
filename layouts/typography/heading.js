9013fce5c7ea7f7d5b0d845d8697ef53134e754f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-10-31 15:10:34 +0300

    Update heading.js

--- layouts/typography/heading.js
+++ layouts/typography/heading.js
@@ -1,1 +1,1 @@
-xxx
+module.exports= `<h3 class="mc-toc-title" dir="ltr" style="text-align: center;display: block;margin: 0;padding: 0;color: #111111;font-family: 'Merriweather Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;font-size: 18px;font-style: normal;font-weight: bold;line-height: 125%;letter-spacing: normal;"><span style="font-family:trebuchet ms,lucida grande,lucida sans unicode,lucida sans,tahoma,sans-serif"><span style="font-size:30px">{content}</span></span></h3>`;
