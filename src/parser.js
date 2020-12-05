52466247656e7e2a8c8b96191fd144cef2b0574d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-06 00:34:19 +0200

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -20,18 +20,3 @@ switch(process.env.PARSE){
     const fileName = "content" + Date.now() + ".html";
     write(fileName, parsedContent.content);
 }
-
-
-//if (process.env.PARSE) {
-//  let html = readFile("main");
-//  const { previewText, content } = parse(FULL_SOURCE);
-//
-//  html = html.replace("{previewText}", previewText);
-//   html = html.replace("{content}", content);
-//   const fileName = "full-template" + Date.now() + ".html";
-//   write(fileName, html);
-// } else {
-//   const parsedContent = parse(CONTENT_SOURCE);
-//   const fileName = "content" + Date.now() + ".html";
-//   write(fileName, parsedContent.content);
-// }
