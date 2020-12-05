517a7e7f139045d95c6dbead34b56ee2f61b07ed
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-06 00:36:42 +0200

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -6,8 +6,32 @@ isFolderExists('generated');
 const FULL_SOURCE = "source/source-full.md";
 const CONTENT_SOURCE = "source/source.md";
 
+function generate_full_template(){
+        let html = readFile("main");
+  const { previewText, content } = parse(FULL_SOURCE);
+
+  html = html.replace("{previewText}", previewText);
+   html = html.replace("{content}", content);
+   const fileName = "full-template" + Date.now() + ".html";
+   write(fileName, html);
+}
+
+function generate_content_only(){
+      const parsedContent = parse(CONTENT_SOURCE);
+    const fileName = "content" + Date.now() + ".html";
+    write(fileName, parsedContent.content);
+}
+
 switch(process.env.PARSE){
   case "full":
+      let html = readFile("main");
+  const { previewText, content } = parse(FULL_SOURCE);
+
+  html = html.replace("{previewText}", previewText);
+   html = html.replace("{content}", content);
+   const fileName = "full-template" + Date.now() + ".html";
+   write(fileName, html);
+    
     break;
   case "react":
     //same as default, but with react components instead.
