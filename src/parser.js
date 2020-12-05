7c8120fa70e9b03f2f3a3da219d81c97cbcb53d1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-06 00:24:42 +0200

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -7,24 +7,31 @@ const FULL_SOURCE = "source/source-full.md";
 const CONTENT_SOURCE = "source/source.md";
 
 switch(process.env.PARSE){
-  case true:
+  case "full":
     break;
-  //case third:
-    //break;  
+  case "react":
+    //same as default, but with react components instead.
+    const parsedContent = parse(CONTENT_SOURCE);
+    const fileName = "content" + Date.now() + ".html";
+    write(fileName, parsedContent.content);
+    break;  
   default:
-    
+    const parsedContent = parse(CONTENT_SOURCE);
+    const fileName = "content" + Date.now() + ".html";
+    write(fileName, parsedContent.content);
 }
 
-if (process.env.PARSE) {
-  let html = readFile("main");
-  const { previewText, content } = parse(FULL_SOURCE);
 
-  html = html.replace("{previewText}", previewText);
-  html = html.replace("{content}", content);
-  const fileName = "full-template" + Date.now() + ".html";
-  write(fileName, html);
-} else {
-  const parsedContent = parse(CONTENT_SOURCE);
-  const fileName = "content" + Date.now() + ".html";
-  write(fileName, parsedContent.content);
-}
+//if (process.env.PARSE) {
+//  let html = readFile("main");
+//  const { previewText, content } = parse(FULL_SOURCE);
+//
+//  html = html.replace("{previewText}", previewText);
+//   html = html.replace("{content}", content);
+//   const fileName = "full-template" + Date.now() + ".html";
+//   write(fileName, html);
+// } else {
+//   const parsedContent = parse(CONTENT_SOURCE);
+//   const fileName = "content" + Date.now() + ".html";
+//   write(fileName, parsedContent.content);
+// }
