a0c8b98dd8b7f7116021902d8b83ec54807b4d5a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-05 23:53:04 +0200

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -3,16 +3,28 @@ const { write, readFile, isFolderExists } = require("./utils");
 
 isFolderExists('generated');
 
+const FULL_SOURCE = "source/source-full.md";
+const CONTENT_SOURCE = "source/source.md";
+
+switch(process.env.PARSE){
+  case true:
+    break;
+  //case third:
+    //break;  
+  default:
+    
+}
+
 if (process.env.PARSE) {
   let html = readFile("main");
-  const { previewText, content } = parse("source/source-full.md");
+  const { previewText, content } = parse(FULL_SOURCE);
 
   html = html.replace("{previewText}", previewText);
   html = html.replace("{content}", content);
   const fileName = "full-template" + Date.now() + ".html";
   write(fileName, html);
 } else {
-  const parsedContent = parse("source/source.md");
+  const parsedContent = parse(CONTENT_SOURCE);
   const fileName = "content" + Date.now() + ".html";
   write(fileName, parsedContent.content);
 }
