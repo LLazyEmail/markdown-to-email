d6367a76c601ea5794a7514a7acd8089b9ccd59c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-08 00:53:24 +0200

    Update parse.js

--- src/parse.js
+++ src/parse.js
@@ -53,34 +53,34 @@ function parse(source) {
     previewText: "",
   };
 
-  replaceMarkdown(REGEXP_HTML_COMMENTS, empty, parsedContent);
-  replaceMarkdownPreviewText(REGEXP_PREVIEW_TEXT, parsedContent);
+  //replaceMarkdown(REGEXP_HTML_COMMENTS, empty, parsedContent);
+  //replaceMarkdownPreviewText(REGEXP_PREVIEW_TEXT, parsedContent);
 
-  replaceMarkdown(REGEXP_STRONG, strong, parsedContent);
+  //replaceMarkdown(REGEXP_STRONG, strong, parsedContent);
   replaceMarkdown(REGEXP_EM, italic, parsedContent);
 
   replaceMarkdown(REGEXP_HEADER, header, parsedContent);
   replaceMarkdown(REGEXP_IMAGE, image, parsedContent);
   replaceMarkdown(REGEXP_LINK, link, parsedContent);
 
-  replaceMarkdown(REGEXP_DEL, del, parsedContent);
-  replaceMarkdown(REGEXP_Q, q, parsedContent);
-  replaceMarkdown(REGEXP_CODE, code, parsedContent);
+  //replaceMarkdown(REGEXP_DEL, del, parsedContent);
+  //replaceMarkdown(REGEXP_Q, q, parsedContent);
+  //replaceMarkdown(REGEXP_CODE, code, parsedContent);
 
-  replaceMarkdown(REGEXP_UL_LIST, ulList, parsedContent);
-  replaceMarkdown(REGEXP_OL_LIST, olList, parsedContent);
+  //replaceMarkdown(REGEXP_UL_LIST, ulList, parsedContent);
+  //replaceMarkdown(REGEXP_OL_LIST, olList, parsedContent);
 
-  replaceMarkdown(REGEXP_BLOCKQUOTE, blockquote, parsedContent);
+  //replaceMarkdown(REGEXP_BLOCKQUOTE, blockquote, parsedContent);
 
-  replaceMarkdown(REGEXP_HR, hr, parsedContent);
-  replaceMarkdown(REGEXP_PARAGRAPH, paragraphWrapper, parsedContent);
-  replaceMarkdown(REGEXP_EMPTY_UL, empty, parsedContent);
-  replaceMarkdown(REGEXP_EMPTY_OL, empty, parsedContent);
-  replaceMarkdown(REGEXP_EMPTY_BLOCKQUOTE, newLine, parsedContent);
+  //replaceMarkdown(REGEXP_HR, hr, parsedContent);
+  //replaceMarkdown(REGEXP_PARAGRAPH, paragraphWrapper, parsedContent);
+  //replaceMarkdown(REGEXP_EMPTY_UL, empty, parsedContent);
+  //replaceMarkdown(REGEXP_EMPTY_OL, empty, parsedContent);
+  //replaceMarkdown(REGEXP_EMPTY_BLOCKQUOTE, newLine, parsedContent);
 
   replaceMarkdown(REGEXP_BR, br, parsedContent);
-  replaceMarkdown(REGEXP_SPONSORSHIP, sponsorship, parsedContent);
-  replaceMarkdown(REGEXP_MEM, mem, parsedContent);
+  //replaceMarkdown(REGEXP_SPONSORSHIP, sponsorship, parsedContent);
+  //replaceMarkdown(REGEXP_MEM, mem, parsedContent);
 
   return parsedContent;
 }
