01d1748750c3f29359f8d248ee821b5b480dbb84
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-08 01:12:52 +0200

    Create paragraph.js

---
+++ layouts/typography/paragraph.js
@@ -0,0 +1,3 @@
+module.exports = `<Paragraph>A statistician from New Zealand once said: Tidy datasets are all alike, but every messy dataset
+        is messy in its own way. Indeed, as data may come in various forms and shapes, sometimes we are
+        inundated with it. </Paragraph>`;
