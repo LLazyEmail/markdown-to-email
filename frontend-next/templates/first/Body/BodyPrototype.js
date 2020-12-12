4f001e3cb503b972ddb818cd5bede0abb5df3aa2
masterbogdan <maiskiychavk@gmail.com>
2020-12-12 16:33:08 +0200

    [finish config file for first template]

--- frontend-next/templates/first/Body/BodyPrototype.js
+++ frontend-next/templates/first/Body/BodyPrototype.js
@@ -9,16 +9,16 @@ import Paragraph from '../../components/typography/paragraph';
 
 const BodyPrototype = ({config}) => {
 
-    const {sponsor, mainTitle, paragraphFirst} = config;
+    const {sponsor, mainTitle, paragraphFirst, memImageLink, paragraphSecond, heading, paragraphThird, memImageLink2, paragraphFourth, heading2, heading3, imageLink, paragraphFive } = config;
     
     return (
         <>
             <Sponsor
-            leftHref="https://bit.ly/30AWyC4"
-            rightHref={sponsor.link}
-            leftSrc={sponsor.imageUrl}
+                leftHref="https://bit.ly/30AWyC4"
+                rightHref={sponsor.link}
+                leftSrc={sponsor.imageUrl}
             >
-            {sponsor.text}
+                {sponsor.text}
             </Sponsor>
             <MainTitle>
                 {mainTitle}
@@ -28,64 +28,47 @@ const BodyPrototype = ({config}) => {
             </Paragraph>
             <Image
                 href="#"
-                src="https://raw.githubusercontent.com/atherdon/newsletters/master/archive/img/memes/6.jpg"
+                src={memImageLink}
                 alt="GIF"
             />
             {' '}
             I
             <Divider />
             <Paragraph>
-            A statistician from New Zealand once said: Tidy datasets are all alike, but every messy dataset
-            is messy in its own way. Indeed, as data may come in various forms and shapes, sometimes we are
-            inundated with it. As a result, our data science team becomes shortsighted and oops..
-            disillusioned by mountains of unworkable data. The only way data specialists can facilitate analysis is
-            by keeping data clean and organized.
+                {paragraphSecond}
             </Paragraph>
             <Heading>
-            <Strong>What is tidy data?</Strong>
+            <Strong>{heading}</Strong>
             </Heading>
             <Paragraph>
-            Essentially, tidy data is a term coined by Hadley Wickham in his Tidy Data paper (remember that
-            statistician from NZ?).
-            He defined tidy data as data that is neatly organized and all set for analysis. This way of organizing
-            allows you to easily produce charts,
-            diagrams, and summary statistics. As it often happens, not all data comes out of the database clean,
-            therefore cleansing it is essential
-            to efficiently analyze it.
+                {paragraphThird}
             </Paragraph>
             <Image
-            href="#"
-            src="https://raw.githubusercontent.com/atherdon/newsletters/master/archive/img/memes/1.png"
+                href="#"
+                src={memImageLink2}
             />
             <Divider />
             <Paragraph>
-            Without further ado, let us break down the principles that allow you keep your data nice and clean.
+                {paragraphFourth}
             </Paragraph>
             <Heading>
-            <Strong>Tidy Data Principles</Strong>
+                <Strong>{heading2}</Strong>
             </Heading>
             <Heading>
-            <Strong>1. Each row is an observational unit.</Strong>
+                <Strong>{heading3}</Strong>
             </Heading>
-            <Image href="#" src="https://raw.githubusercontent.com/atherdon/newsletters/master/archive/img/image1.png" />
+            <Image href="#" src={imageLink} />
             <Divider />
             <Paragraph>
-            We’ll start with one of the basic principles. When you are giving your data the once-over, you should
-            make sure each row contains an observation.
-            By definition, observation is the individual unit under question. If we look at the table above, an
-            observational unit could be called ‘people’.
-            You can see that each person has an individual row on the table and all of the information for that
-            person is in the same row.
-            Observations are included in rows, variables are represented as columns and there is only one
-            observational unit per table. Now THIS is tidy data.
+                {paragraphFive}
             </Paragraph>
             <Divider />
             <Sponsor
-            leftHref="https://bit.ly/30AWyC4"
-            rightHref="https://bit.ly/30AWyC4"
-            leftSrc="https://raw.githubusercontent.com/atherdon/newsletters/master/archive/logos/thematic/2020/September/flatfile-logo-black-small-vertical.png"
+                leftHref="https://bit.ly/30AWyC4"
+                rightHref={sponsor.link}
+                leftSrc={sponsor.imageUrl}
             >
-            Ship the data importer you always dreamed of
+                {sponsor.text}
             </Sponsor>
         </>
     )
