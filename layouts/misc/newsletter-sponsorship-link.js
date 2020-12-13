33e00e691a597a52c326f35113cac93015431012
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-13 02:01:10 +0200

    Update newsletter-sponsorship-link.js

--- layouts/misc/newsletter-sponsorship-link.js
+++ layouts/misc/newsletter-sponsorship-link.js
@@ -1,8 +1,3 @@
 const config = require('../config');
 
-module.exports = `<div style="text-align: center;">
-<br>
-<a href="${config.contact}" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">
-  Click Here To Sponsor A Newsletter by Hacker Noon
-</a>
-</div>`;
+module.exports = `<NewsletterSponsorshipLink />`;
