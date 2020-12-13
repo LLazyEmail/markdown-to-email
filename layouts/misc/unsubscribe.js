e6f7eb2eec588aa81bae4885a1b78de667778bb8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-13 02:01:47 +0200

    Update unsubscribe.js

--- layouts/misc/unsubscribe.js
+++ layouts/misc/unsubscribe.js
@@ -1,5 +1,3 @@
 const config = require('../config');
 
-module.exports = `<a href="${config.unsubscribe}" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">
-unsubscribe
-</a>`;
+module.exports = `<Unsubscribe />`;
