6c331ded6e3be8c6245655239e258ccfc6426b1b
Meruzhan Kiloyan <meruzh.kiloyan@mail.ru>
2020-12-17 17:09:25 +0400

    tests for Address components

--- frontend-next/components/misc/address/address.test.js
+++ frontend-next/components/misc/address/address.test.js
@@ -1,30 +1,11 @@
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Address from './index';
-
-const _reactDom = require('react-dom');
-
-const _reactDom2 = _interopRequireDefault(_reactDom);
-
-const _index = require('./index');
-
-const _index2 = _interopRequireDefault(_index);
-
-const _react = require('react');
-
-const _react2 = _interopRequireDefault(_react);
-
-const _react3 = require('@testing-library/react');
-
-const _reactTestRenderer = require('react-test-renderer');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import Address from './Address';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
 configure({ adapter: new Adapter() });
-
-afterEach(() => {
-  (0, _react3.cleanup)();
-});
 
 describe('address/index component', () => {
 
@@ -37,21 +18,21 @@ describe('address/index component', () => {
         const wrapper = shallow(<Address />);
         const elem = wrapper.find('strong');
         expect(elem.text()).toEqual('Our mailing address is:');
-    });
+    })
 
     it('<Address > element renders correct address', () => {
         const wrapper = shallow(<Address />);
         expect(wrapper.text()).toEqual('Our mailing address is:PO Box 2206, Edwards CO, 81632, U.S.A.');
     });
 
-  test('renders without crashing', () => {
-    const div = document.createElement('div');
-    _reactDom2.default.render(_react2.default.createElement(_index2.default, null), div);
-    _reactDom2.default.unmountComponentAtNode(div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Address />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 
-  test('renders correctly', () => {
-    const tree = (0, _reactTestRenderer.create)(_react2.default.createElement(_index2.default, null));
-    expect(tree.toJSON()).toMatchSnapshot();
-  });
+    it('renders correctly', () => {
+        const wrapper = renderer.create(<Address />).toJSON();
+        expect(wrapper).toMatchSnapshot();
+    });
 });
