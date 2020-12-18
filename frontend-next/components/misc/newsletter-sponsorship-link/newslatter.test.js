804a70d0c95051a9e9456bad8c2d7ad180ba0749
Meruzh Kiloyan <68124376+coder-do@users.noreply.github.com>
2020-12-18 12:03:29 +0400

    Update newslatter.test.js

--- frontend-next/components/misc/newsletter-sponsorship-link/newslatter.test.js
+++ frontend-next/components/misc/newsletter-sponsorship-link/newslatter.test.js
@@ -3,31 +3,31 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
-import Newsletter from './index';
+import NewsletterSponsorshipLink from './index';
 
 configure({ adapter: new Adapter() });
 
 describe('newslatter/index component', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Newsletter />, div);
+        ReactDOM.render(<NewsletterSponsorshipLink />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
     it('renders correctly (Snapshot)', () => {
-        const wrapper = renderer.create(<Newsletter />).toJSON();
+        const wrapper = renderer.create(<NewsletterSponsorshipLink />).toJSON();
         expect(wrapper).toMatchSnapshot();
     });
 
     it('Checking <a> attributes', () => {
-        const wrapper = shallow(<Newsletter />);
+        const wrapper = shallow(<NewsletterSponsorshipLink />);
         const a = wrapper.find('a');
         expect(a.props()).toHaveProperty('data-testid', 'newslatterHrefTest');
         expect(a.props()).toHaveProperty('target', '_blank');
     });
 
     it('Checking <a> text and link', () => {
-        const wrapper = shallow(<Newsletter />);
+        const wrapper = shallow(<NewsletterSponsorshipLink />);
         const a = wrapper.find('a');
         expect(a.text()).toEqual(`Click Here To Sponsor A Newsletter by Hacker Noon`);
         expect(a.props()).toHaveProperty('href' ,`https://sponsor.hackernoon.com/contact`);
