163fa1e19d8dbfcc8a808de93cf5117aafabd5a8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2020-12-20 17:19:50 +0200

    Update callbacks.js

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,5 +1,5 @@
 const { replaceHTMLWrapper } = require("./helpers");
-const {newLine} =require('./utils');
+const { newLine } =require('./utils');
 
 const italic = "$1<em>$3</em>$4";
 const del = "<del>$1</del>";
