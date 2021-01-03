1a1ad38875a836a3e2cffb5be257cac757cea799
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-01-04 01:14:46 +0200

    Update converter-object.js

--- src/converter-object.js
+++ src/converter-object.js
@@ -46,59 +46,6 @@ const {
 //const layouts = require("atherdon-newsletter-constants");
 
 
-const ConverterTypography = {
-     heading: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       image: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       italic: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       link: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       list: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       listItem: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       mainTitle: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       paragraph: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       strong: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   },
-       subtitle: {
-//     key: "REGEXP_H3",
-//     value: 1,
-//     title: "xxx",
-   }
-};
-
 // const Converter = {
 //   REGEXP_H3: {
 //     key: "REGEXP_H3",
