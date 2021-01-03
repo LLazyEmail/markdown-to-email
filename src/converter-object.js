3cea822d112c479cd44b1163273d65ed848c0cd1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-01-04 00:26:08 +0200

    Create converter-object.js

---
+++ src/converter-object.js
@@ -0,0 +1,55 @@
+
+// const Converter = {
+//   REGEXP_H3: {
+//     key: "REGEXP_H3",
+//     value: 1,
+//     title: "xxx",
+//   },
+//   REGEXP_H2: {
+//     key: "REGEXP_H2",
+//     value: 2,
+//     title: "xxx",
+//   },
+//   REGEXP_H1: {
+//     key: "REGEXP_H1",
+//     value: 3,
+//     title: "xxx",
+//   },
+//   REGEXP_BLOCKQUOTE: {
+//     key: "REGEXP_BLOCKQUOTE",
+//     value: 4,
+//     title: "xxx",
+//   },
+//   REGEXP_B: {
+//     key: "REGEXP_B",
+//     value: 5,
+//     title: "xxx",
+//   },
+//   REGEXP_I: {
+//     key: "REGEXP_I",
+//     value: 6,
+//     title: "xxx",
+//   },
+//   REGEXP_IMG: {
+//     key: "REGEXP_H3",
+//     value: 7,
+//     title: "xxx",
+//   },
+//   REGEXP_A: {
+//     key: "REGEXP_A",
+//     value: 8,
+//     title: "xxx",
+//   },
+//   REGEXP_BR: {
+//     key: "REGEXP_BR",
+//     value: 9,
+//     title: "xxx",
+//   },
+// };
+
+// example Converter.REGEXP_BR
+
+// how to make a loop?
+// Object.keys(Converter).map(key => {
+//   console.log(Converter[key])
+// })
