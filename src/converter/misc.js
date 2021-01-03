90fd925136ac160568f587787358c6b0fb59a523
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-01-04 01:53:28 +0200

    Update misc.js

--- src/converter/misc.js
+++ src/converter/misc.js
@@ -18,32 +18,22 @@ const {
 
 const {
   REGEXP_HEADER,
-  REGEXP_IMAGE,
-  REGEXP_LINK,
-  REGEXP_STRONG,
+  
   REGEXP_DEL,
   REGEXP_Q,
   REGEXP_CODE,
-  REGEXP_UL_LIST,
-  REGEXP_OL_LIST,
-  REGEXP_BLOCKQUOTE,
-  REGEXP_HR,
-  REGEXP_PARAGRAPH,
-  REGEXP_EMPTY_UL,
-  REGEXP_EMPTY_OL,
-  REGEXP_BR,
-  REGEXP_EMPTY_BLOCKQUOTE,
+  
   REGEXP_EM,
   REGEXP_SPONSORSHIP,
   REGEXP_HTML_COMMENTS,
   REGEXP_MEM,
-  REGEXP_PREVIEW_TEXT,
+  REGEXP_PREVIEW_TEXT
 } = require("../constants");
 
 //const layouts = require("atherdon-newsletter-constants");
 
 
-const ConverterTypography = {
+const ConverterMisc = {
      heading: {
 //     key: "REGEXP_H3",
 //     value: 1,
