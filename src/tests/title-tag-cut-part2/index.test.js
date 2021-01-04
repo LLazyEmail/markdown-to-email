e6cc567799ed446abb675beed17a663cf79b9177
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-01-04 15:04:52 +0200

    Update index.test.js

--- src/tests/title-tag-cut-part2/index.test.js
+++ src/tests/title-tag-cut-part2/index.test.js
@@ -1,1 +1,21 @@
-xxx
+// const { write, readSourceFile } = require("../../utils");
+// const { header } = require("../../callbacks");
+// const { replaceMarkdown } = require("../../helpers");
+// const { REGEXP_HEADER } = require("../../constants");
+
+// const outFolder = "src/tests/_generated";
+
+// describe("testing h1-inside-the-body", () => {
+//   it("renders h1-inside-the-body", () => {
+//     let markdown = readSourceFile("src/tests/h1-inside-the-body/content.md");
+//     let parsedContent = {
+//       content: markdown,
+//     };
+
+//     replaceMarkdown.call(parsedContent, REGEXP_HEADER, header);
+
+//     const fileName = "h1-inside-the-body.html";
+//     write(fileName, parsedContent.content, outFolder);
+//     expect(1).toBe(1);
+//   });
+// });
