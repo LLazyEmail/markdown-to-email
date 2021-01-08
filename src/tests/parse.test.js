95624b33b31df20c4e49f97bbeb01142b939c537
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-01-08 17:15:47 +0400

    added tests

---
+++ src/tests/parse.test.js
@@ -0,0 +1,8 @@
+const { parse } = require('../parse');
+
+describe('testing parse.js', () => {
+    test('parse should return an object(empty or with parameters)', () => {
+        const p = parse('source/source.md');
+        expect(typeof p).toBe('object');
+    })
+})
