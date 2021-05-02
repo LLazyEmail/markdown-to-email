9aa830e26ceee2e968d9ad71836db3fda9d708d7
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-05-02 09:12:30 +0400

    replaced sponsor link

--- package/generator-react-components/dist/index.es.js
+++ package/generator-react-components/dist/index.es.js
@@ -1,5 +1,5 @@
 var config = {
-  contact: "https://sponsor.hackernoon.com/contact",
+  contact: "https://sponsor.hackernoon.com/newsletter?ref=noonifications.tech",
   mailingAddress: "PO Box 2206, Edwards CO, 81632, U.S.A.",
   unsubscribe: '#'
 };
--- package/generator-react-components/dist/index.iife.js
+++ package/generator-react-components/dist/index.iife.js
@@ -2,7 +2,7 @@ var newsletterLayoutsReact = (function () {
   'use strict';
 
   var config = {
-    contact: "https://sponsor.hackernoon.com/contact",
+    contact: "https://sponsor.hackernoon.com/newsletter?ref=noonifications.tech",
     mailingAddress: "PO Box 2206, Edwards CO, 81632, U.S.A.",
     unsubscribe: '#'
   };
