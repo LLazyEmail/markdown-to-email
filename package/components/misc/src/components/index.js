bab11276126c24b1b379963e9a82d260dd55875e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-05-02 21:49:46 +0000

    upd

--- package/components/misc/src/components/index.js
+++ package/components/misc/src/components/index.js
@@ -1,1 +1,8 @@
-export default {}
+import Address from './address';
+import Copyright from './copyrights';
+import NewsletterSponsorshipLink from './newsletter-sponsorship-link';
+import Unsubscribe from './unsubscribe';
+
+export default {
+    Address, Copyright, NewsletterSponsorshipLink, Unsubscribe
+} ;
