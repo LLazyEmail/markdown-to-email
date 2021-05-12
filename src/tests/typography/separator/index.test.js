b961e2430fbbcf52af6ede6b9cd0b566245ba727
Vadim <vadim.putrov@gmail.com>
2021-05-12 20:33:37 +0300

    add test for separator

---
+++ src/tests/typography/separator/index.test.js
@@ -0,0 +1,22 @@
+const { REGEXP_SEPARATOR } = require('atherdon-newsletter-constants');
+const { resolve } = require('path');
+const { write, readSourceFile } = require('@root/utils');
+const { separator } = require('@root/callbacks-custom');
+const { replaceMarkdown } = require('@root/helpers');
+
+const root = resolve(__dirname, '');
+const outFolder = resolve('src/tests', 'directory', '../_generated');
+
+describe('testing separator', () => {
+  it('renders separator', () => {
+    const markdown = readSourceFile(`${root}/content.md`);
+    const parsedContent = {
+      content: markdown,
+    };
+
+    replaceMarkdown.call(parsedContent, REGEXP_SEPARATOR, separator);
+    const fileName = 'separator.html';
+    write(fileName, parsedContent.content, outFolder);
+    expect(1).toBe(1);
+  });
+});
