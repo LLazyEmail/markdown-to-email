4f490becb6685fe499dc23ea5a4e92ab64a09c3d
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-05-15 18:05:16 +0400

    added some tests

---
+++ src/tests/typography/lists/list3/index.test.js
@@ -0,0 +1,23 @@
+const { REGEXP_OL_LIST } = require('atherdon-newsletter-constants');
+const { resolve } = require('path');
+const { write, readSourceFile } = require('@root/utils');
+const { olList } = require('@root/callbacks');
+const { replaceMarkdown } = require('@root/helpers');
+
+const root = resolve(__dirname, '');
+const outFolder = resolve('src/tests', 'directory', '../_generated');
+
+describe('testing list3', () => {
+    it('renders list3 correctly', () => {
+        const markdown = readSourceFile(`${root}/content.md`);
+        const parsedContent = {
+            content: markdown,
+        };
+
+        replaceMarkdown.call(parsedContent, REGEXP_OL_LIST, olList);
+
+        const fileName = 'list3.html';
+        write(fileName, parsedContent.content, outFolder);
+        expect(1).toBe(1);
+    });
+});
