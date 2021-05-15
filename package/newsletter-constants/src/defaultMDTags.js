ece83de5041427261767d0a88df3859ea82133a6
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-05-15 16:33:25 +0000

    publishing updates in constants

--- package/newsletter-constants/src/defaultMDTags.js
+++ package/newsletter-constants/src/defaultMDTags.js
@@ -14,19 +14,18 @@ const REGEXP_DEL = /\~\~(.*?)\~\~/g;
 const REGEXP_Q = /\:\"(.*?)\"\:/g;
 const REGEXP_CODE = /`(.*?)`/g;
 
-const REGEXP_UL_LIST = new RegExp(
-  `${newLine}(((\\s{4})?\\*(.*?)${newLine}){1,})`,
-  "g"
-);
-const REGEXP_OL_LIST = new RegExp(`${newLine}[0-9]+\\.(.*)`, "g");
+
+
+
 
 const REGEXP_BLOCKQUOTE = new RegExp(`${newLine}(&gt;|\\>)(.*)`, "g");
 const REGEXP_HR = new RegExp(`${newLine}-{5,}`, "g");
 
 const REGEXP_PARAGRAPH = new RegExp(`${newLine}(.+?)${newLine}`, "g");
 
-const REGEXP_EMPTY_UL = /<\/ul>\s?<ul>/g;
-const REGEXP_EMPTY_OL = /<\/ol>\s?<ol>/g;
+
+
+
 const REGEXP_BR = new RegExp(`((${newLine}){2,})`, "g");
 
 const REGEXP_EMPTY_BLOCKQUOTE = /<\/blockquote><blockquote>/g;
@@ -41,13 +40,11 @@ export {
   REGEXP_DEL,
   REGEXP_Q,
   REGEXP_CODE,
-  REGEXP_UL_LIST,
-  REGEXP_OL_LIST,
+
   REGEXP_BLOCKQUOTE,
   REGEXP_HR,
   REGEXP_PARAGRAPH,
-  REGEXP_EMPTY_UL,
-  REGEXP_EMPTY_OL,
+
   REGEXP_BR,
   REGEXP_EMPTY_BLOCKQUOTE,
   REGEXP_EM,
