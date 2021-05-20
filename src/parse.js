c04dd24784407df8179142cc7f2e6953ee3f654a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-05-20 18:31:04 +0000

    updates

--- src/parse.js
+++ src/parse.js
@@ -6,6 +6,12 @@ const { replaceMarkdown, replaceMarkdownPreviewText } = require('./helpers');
 
 const ReplacerObj = require('./replace');
 
+const { PlainCallbacks } = require("atherdon-callbacks")
+
+console.log(PlainCallbacks);
+
+// aaa = PlainCallbacks
+
 // @todo update this method. I'm sure it can be improved.
 function parse(source) {
   const markdown = readSourceFile(source);
@@ -23,33 +29,62 @@ function parse(source) {
     },
   };
 
-  ReplacerObj.replaceMDBinded = replaceMarkdown.bind(state);
-  ReplacerObj.replaceMDBindedPreviewText =
+  PlainCallbacks.replaceMDBinded = replaceMarkdown.bind(state);
+  PlainCallbacks.replaceMDBindedPreviewText =
     replaceMarkdownPreviewText.bind(state);
 
-  ReplacerObj.comments();
-  ReplacerObj.strong();
-  ReplacerObj.em();
-  ReplacerObj.header();
-  ReplacerObj.image();
-  ReplacerObj.link();
-  ReplacerObj.del();
-  ReplacerObj.q();
-  ReplacerObj.code();
-  ReplacerObj.ul();
-  ReplacerObj.ol();
-  ReplacerObj.blockquote();
-  ReplacerObj.hr();
-  ReplacerObj.paragraph();
-  ReplacerObj.emptyUl();
-  ReplacerObj.emptyOl();
+  PlainCallbacks.comments();
+  PlainCallbacks.strong();
+  PlainCallbacks.em();
+  PlainCallbacks.header();
+  PlainCallbacks.image();
+  PlainCallbacks.link();
+  PlainCallbacks.del();
+  PlainCallbacks.q();
+  PlainCallbacks.code();
+  PlainCallbacks.ul();
+  PlainCallbacks.ol();
+  PlainCallbacks.blockquote();
+  PlainCallbacks.hr();
+  PlainCallbacks.paragraph();
+  PlainCallbacks.emptyUl();
+  PlainCallbacks.emptyOl();
 
   // this line is generating an error
-  ReplacerObj.emptyBlockquote();
-  ReplacerObj.br();
-  ReplacerObj.sponsorship();
-  ReplacerObj.memes();
-  ReplacerObj.separator();
+  PlainCallbacks.emptyBlockquote();
+  PlainCallbacks.br();
+  PlainCallbacks.sponsorship();
+  PlainCallbacks.memes();
+  PlainCallbacks.separator();
+
+
+//   ReplacerObj.replaceMDBinded = replaceMarkdown.bind(state);
+//   ReplacerObj.replaceMDBindedPreviewText =
+//     replaceMarkdownPreviewText.bind(state);
+
+//   ReplacerObj.comments();
+//   ReplacerObj.strong();
+//   ReplacerObj.em();
+//   ReplacerObj.header();
+//   ReplacerObj.image();
+//   ReplacerObj.link();
+//   ReplacerObj.del();
+//   ReplacerObj.q();
+//   ReplacerObj.code();
+//   ReplacerObj.ul();
+//   ReplacerObj.ol();
+//   ReplacerObj.blockquote();
+//   ReplacerObj.hr();
+//   ReplacerObj.paragraph();
+//   ReplacerObj.emptyUl();
+//   ReplacerObj.emptyOl();
+
+//   // this line is generating an error
+//   ReplacerObj.emptyBlockquote();
+//   ReplacerObj.br();
+//   ReplacerObj.sponsorship();
+//   ReplacerObj.memes();
+//   ReplacerObj.separator();
 
   //i'm adding it only because error warning didnt return red stuff
   checkErrors(state.errors);
