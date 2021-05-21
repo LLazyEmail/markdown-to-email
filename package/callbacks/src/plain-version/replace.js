7fac47cf1e5d2256d35eb90d9be79eef9bc745be
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-05-21 07:37:28 +0000

    upd

--- package/callbacks/src/plain-version/replace.js
+++ package/callbacks/src/plain-version/replace.js
@@ -1,59 +1,7 @@
-// This file is just my attempt to simplify
-// a process of reading and debugging our code
-// i'm creating a set of methods, that have a simple name.
-// it will help us to move forward.
-// import {
-//   REGEXP_HEADER,
-//   REGEXP_IMAGE,
-//   REGEXP_LINK,
-//   REGEXP_STRONG,
-//   REGEXP_DEL,
-//   REGEXP_Q,
-//   REGEXP_CODE,
-//   REGEXP_UL_LIST,
-//   REGEXP_OL_LIST,
-//   REGEXP_BLOCKQUOTE,
-//   REGEXP_HR,
-//   REGEXP_PARAGRAPH,
-//   REGEXP_EMPTY_UL,
-//   REGEXP_EMPTY_OL,
-//   REGEXP_BR,
-//   REGEXP_EMPTY_BLOCKQUOTE,
-//   REGEXP_EM,
-//   REGEXP_SPONSORSHIP,
-//   REGEXP_HTML_COMMENTS,
-//   REGEXP_MEM,
-//   REGEXP_PREVIEW_TEXT,
-//   REGEXP_SEPARATOR,
-// } from 'atherdon-newsletter-constants';
 
 import * as constants from 'atherdon-newsletter-constants'
 
 import { newLine } from '../helpers';
-
-// import {
-//   strong,
-//   link,
-//   blockquote,
-//   mem,
-//   header,
-//   italic,
-//   del,
-//   q,
-//   code,
-//   hr,
-//   empty,
-
-//   ulList,
-//   olList,
-
-//   image,
-//   paragraphWrapper,
-//   sponsorship,
-//   br,
-
-//   separator
-// } from './index';
 
 import {
   strong,
@@ -102,8 +50,80 @@ class Replace {
         };
 
         this.em = () => {
-            this.replaceMDBinded(this._constants.REGEXP_EM, italic);
+            this.replaceMDBinded(this._constants.this._constants.REGEXP_EM, italic);
         }
+
+        // this.header = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_HEADER, header);
+// };
+
+// this.image = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_IMAGE, image);
+// };
+
+// this.link = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_LINK, link);
+// };
+
+// this.del = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_DEL, del);
+// };
+
+// this.q = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_Q, q);
+// };
+
+// this.code = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_CODE, code);
+// };
+
+// this.ul = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_UL_LIST, ulList);
+// };
+
+// this.ol = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_OL_LIST, olList);
+// };
+
+// this.blockquote = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_BLOCKQUOTE, blockquote);
+// };
+
+// this.hr = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_HR, hr);
+// };
+
+// this.paragraph = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_PARAGRAPH, paragraphWrapper);
+// };
+
+// this.emptyUl = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_EMPTY_UL, empty);
+// };
+
+// this.emptyOl = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_EMPTY_OL, empty);
+// };
+
+// this.emptyBlockquote = function () {
+//   // this line is generating an error
+//   this.replaceMDBinded(this._constants.REGEXP_EMPTY_BLOCKQUOTE, newLine);
+// };
+
+// this.br = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_BR, br);
+// };
+// this.sponsorship = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_SPONSORSHIP, sponsorship);
+// };
+
+// this.memes = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_MEM, mem);
+// };
+
+// this.separator = function () {
+//   this.replaceMDBinded(this._constants.REGEXP_SEPARATOR, separator);
+// };
 
     }
 
@@ -112,80 +132,10 @@ class Replace {
 
 
 
-// Replacer.prototype.em = function () {
+// this.em = function () {
 //   this.replaceMDBinded(REGEXP_EM, italic);
 // };
 
-// Replacer.prototype.header = function () {
-//   this.replaceMDBinded(REGEXP_HEADER, header);
-// };
 
-// Replacer.prototype.image = function () {
-//   this.replaceMDBinded(REGEXP_IMAGE, image);
-// };
-
-// Replacer.prototype.link = function () {
-//   this.replaceMDBinded(REGEXP_LINK, link);
-// };
-
-// Replacer.prototype.del = function () {
-//   this.replaceMDBinded(REGEXP_DEL, del);
-// };
-
-// Replacer.prototype.q = function () {
-//   this.replaceMDBinded(REGEXP_Q, q);
-// };
-
-// Replacer.prototype.code = function () {
-//   this.replaceMDBinded(REGEXP_CODE, code);
-// };
-
-// Replacer.prototype.ul = function () {
-//   this.replaceMDBinded(REGEXP_UL_LIST, ulList);
-// };
-
-// Replacer.prototype.ol = function () {
-//   this.replaceMDBinded(REGEXP_OL_LIST, olList);
-// };
-
-// Replacer.prototype.blockquote = function () {
-//   this.replaceMDBinded(REGEXP_BLOCKQUOTE, blockquote);
-// };
-
-// Replacer.prototype.hr = function () {
-//   this.replaceMDBinded(REGEXP_HR, hr);
-// };
-
-// Replacer.prototype.paragraph = function () {
-//   this.replaceMDBinded(REGEXP_PARAGRAPH, paragraphWrapper);
-// };
-
-// Replacer.prototype.emptyUl = function () {
-//   this.replaceMDBinded(REGEXP_EMPTY_UL, empty);
-// };
-
-// Replacer.prototype.emptyOl = function () {
-//   this.replaceMDBinded(REGEXP_EMPTY_OL, empty);
-// };
-
-// Replacer.prototype.emptyBlockquote = function () {
-//   // this line is generating an error
-//   this.replaceMDBinded(REGEXP_EMPTY_BLOCKQUOTE, newLine);
-// };
-
-// Replacer.prototype.br = function () {
-//   this.replaceMDBinded(REGEXP_BR, br);
-// };
-// Replacer.prototype.sponsorship = function () {
-//   this.replaceMDBinded(REGEXP_SPONSORSHIP, sponsorship);
-// };
-
-// Replacer.prototype.memes = function () {
-//   this.replaceMDBinded(REGEXP_MEM, mem);
-// };
-
-// Replacer.prototype.separator = function () {
-//   this.replaceMDBinded(REGEXP_SEPARATOR, separator);
-// };
 
 export default new Replace(constants);
