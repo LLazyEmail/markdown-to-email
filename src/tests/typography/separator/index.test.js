480d56ec5dbc4f32383771d72bc5b65b3cd9d630
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-05-26 19:43:15 +0400

    changed import separator

--- src/tests/typography/separator/index.test.js
+++ src/tests/typography/separator/index.test.js
@@ -2,9 +2,10 @@ const { REGEXP_SEPARATOR } = require('atherdon-newsletter-constants');
 const { resolve } = require('path');
 const { write, readSourceFile } = require('@root/utils');
 // const { separator } = require('@root/callbacks-custom');
-const { replaceMarkdown } = require('@root/helpers');
+// const { replaceMarkdown } = require('@root/helpers');
 
 const { PlainCallbacks } = require("atherdon-callbacks");
+const { replaceMarkdown } = require("atherdon-callbacks");
 
 const root = resolve(__dirname, '');
 const outFolder = resolve('src/tests', 'directory', '../_generated');
