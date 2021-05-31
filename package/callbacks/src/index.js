99f3598765a6f741a1710710078c775938c3391f
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-05-31 12:57:46 +0400

    fixes

--- package/callbacks/src/index.js
+++ package/callbacks/src/index.js
@@ -2,10 +2,13 @@ import PlainCallbacks from './plain-version/index';
 import ReactCallbacks from './react/index';
 import Replacer from './plain-version/replace';
 import ReactReplacer from './react/react-replace';
-import * as Helpers from './helpers';
+import { newLine, replaceHTMLWrapper, replaceMarkdown, replaceMarkdownPreviewText } from './helpers';
 export {
   PlainCallbacks,
   ReactCallbacks, 
   Replacer,
-  Helpers
+  newLine,
+  replaceHTMLWrapper,
+  replaceMarkdown,
+  replaceMarkdownPreviewText
 };
