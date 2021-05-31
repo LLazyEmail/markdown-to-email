03ff8bb1b43e964763dc41c0b447fd8648e22b5f
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-05-31 12:54:31 +0400

    updated callbacks package

--- package/callbacks/src/index.js
+++ package/callbacks/src/index.js
@@ -2,10 +2,10 @@ import PlainCallbacks from './plain-version/index';
 import ReactCallbacks from './react/index';
 import Replacer from './plain-version/replace';
 import ReactReplacer from './react/react-replace';
-import { replaceMarkdown } from './helpers'
+import * as Helpers from './helpers';
 export {
   PlainCallbacks,
   ReactCallbacks, 
   Replacer,
-  replaceMarkdown
+  Helpers
 };
