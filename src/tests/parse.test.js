73d95241a2b93eb568133a7ddfbebd5f53ab7e26
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-06-01 18:31:06 +0400

    fixed tests

--- src/tests/parse.test.js
+++ src/tests/parse.test.js
@@ -1,8 +1,28 @@
+const { isObject } = require('lodash');
 const { parse } = require('../parse');
 
+const FULL_SOURCE = 'source/source-full.md';
+
 describe('testing parse.js', () => {
-    test('parse should return an object(empty or with parameters)', () => {
-        const p = parse('source/source.md');
-        expect(typeof p).toBe('object');
-    })
-})
+  const p = parse(FULL_SOURCE);
+
+  test('parse should return an object', () => {
+    expect(isObject(p)).toBe(true);
+  });
+
+  test('parse should return an object with content key', () => {
+    expect(p.hasOwnProperty('content')).toBe(true);
+  });
+
+  test('parse should return an object with errors key', () => {
+    expect(p.hasOwnProperty('errors')).toBe(true);
+  });
+
+  test('parse should return an object with previewText key', () => {
+    expect(p.hasOwnProperty('previewText')).toBe(true);
+  });
+
+  test('parse should return an object with warnings key', () => {
+    expect(p.hasOwnProperty('warnings')).toBe(true);
+  });
+});
