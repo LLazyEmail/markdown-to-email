9491875a52a6c03f5f71fcc40a991604cdd2ba9e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-06-04 10:22:15 +0000

    re-organization

---
+++ package/layouts/typography/typography/index.js
@@ -0,0 +1,27 @@
+import { heading } from './heading';
+import { image } from './partials/image';
+import { italic } from './partials/italic';
+import { link } from './partials/link';
+import { list } from './partials/list';
+import { listItem } from './partials/listItem';
+import { title } from './partials/mainTitle';
+import { paragraph } from './partials/paragraph';
+import { strong } from './partials/strong';
+import { subtitle } from './partials/subtitle';
+import { separator } from './partials/separator';
+
+const typography = {
+  heading,
+  image,
+  italic,
+  link,
+  list,
+  listItem,
+  title,
+  paragraph,
+  strong,
+  subtitle,
+  separator
+};
+
+export default typography;
