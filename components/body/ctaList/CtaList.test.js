b75f9f1211410d512d99223f5aff8cfa9ed47049
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-06-10 19:13:21 +0400

    fixed tests

--- components/body/ctaList/CtaList.test.js
+++ components/body/ctaList/CtaList.test.js
@@ -8,14 +8,32 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('CtaList/index component', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<CtaList />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<CtaList />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-    it('renders correctly', () => {
-        const tree = create(<CtaList />);
-        expect(tree.toJSON()).toMatchSnapshot();
-    });
+  it('renders correctly', () => {
+    const tree = create(<CtaList />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('CtaList should contain a list of elements', () => {
+    const wrapper = shallow(<CtaList />);
+    const list = wrapper.find('ol').exists();
+    expect(list).toEqual(true);
+  })
+
+  it('ol in CtaList should contain at least 1 element', () => {
+    const wrapper = shallow(<CtaList />);
+    const list = wrapper.find('li').length > 1;
+    expect(list).toEqual(true);
+  })
+
+  it('CtaList header', () => {
+    const wrapper = shallow(<CtaList />);
+    const list = wrapper.find('strong');
+    expect(list.render().text()).toEqual("Ready to Claim Your Internet Name on Hacker Noon?");
+  })
 });
