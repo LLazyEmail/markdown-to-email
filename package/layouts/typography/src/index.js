5dc3adf525ffcac6ecc607a11f832e996a5140a1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-06-13 14:40:40 +0000

    updates

--- package/layouts/typography/src/index.js
+++ package/layouts/typography/src/index.js
@@ -1,13 +1,13 @@
-import { heading } from './heading';
-import { image } from './partials/image';
-import { italic } from './partials/italic';
-import { link } from './partials/link';
-import { list } from './partials/list';
+import { heading }  from './partials/heading';
+import { image }    from './partials/image';
+import { italic }   from './partials/italic';
+import { link }     from './partials/link';
+import { list }     from './partials/list';
 import { listItem } from './partials/listItem';
-import { title } from './partials/mainTitle';
+import { title }    from './partials/mainTitle';
 import { paragraph } from './partials/paragraph';
-import { strong } from './partials/strong';
-import { subtitle } from './partials/subtitle';
+import { strong }    from './partials/strong';
+import { subtitle }  from './partials/subtitle';
 import { separator } from './partials/separator';
 
 const typography = {
