0bb4958552b330274713be10c1c6f34747570972
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-12 14:00:39 +0300

    upd

--- package/callbacks/src/index.js
+++ package/callbacks/src/index.js
@@ -1,7 +1,9 @@
 import PlainCallbacks from './plain-version/index';
 import ReactCallbacks from './react/index';
+
 import Replacer from './plain-version/replace';
 import ReactReplacer from './react/react-replace';
+
 import {
   newLine, 
   replaceHTMLWrapper, 
