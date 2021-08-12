e38138a041047a2fa2e925a9f95b253d4ed54c50
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-12 16:03:54 +0300

    renaming things

--- package/hn/typography/src/index.js
+++ package/hn/typography/src/index.js
@@ -1,7 +1,7 @@
 /* eslint import/extensions: 0 */
 
-import AnExample from './AnExample/index.jsx';
-import AnotherExample from './AnotherExample/index.jsx';
+// import AnExample from './AnExample/index.jsx';
+// import AnotherExample from './AnotherExample/index.jsx';
 
 import Address from './address/Address.jsx';
 import Copyright from './copyrights/Copyrights.jsx';
@@ -10,9 +10,20 @@ import NewsletterSponsorshipLink from './newsletter-sponsorship-link/Newsletter.
 import Unsubscribe from './unsubscribe/Unsubscribe.jsx';
 
 
-export {
-  AnExample,
-  AnotherExample,
+import from './divider/Divider.jsx';
+import from './heading/Heading.jsx';
+import from './image/Image.jsx';
+import from './italic/Italic.jsx';
+import from './link/Link.jsx';
+import from './list/List.jsx';
+import from './mainTitle/mainTitle.jsx';
+import from './paragraph/Paragraph.jsx';
+import from './separator/Separator.jsx';
+import from './strong/Strong.jsx';
+import from './subtitle/Subtitle.jsx';
 
-  Address, Copyright, headStyles, NewsletterSponsorshipLink, Unsubscribe
+
+
+export {
+
 };
