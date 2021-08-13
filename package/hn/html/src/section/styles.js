0f418dae952889217700dc756ea9ac460f5cf358
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-13 15:55:17 +0300

    upd

---
+++ package/hn/html/src/section/styles.js
@@ -0,0 +1,37 @@
+export const styles = {
+  textBlock: {
+    minWidth: '100%',
+    borderCollapse: 'collapse',
+    msoTableLspace: '0pt',
+    msoTableRspace: '0pt',
+    msTextSizeAdjust: '100%',
+    WebkitTextSizeAdjust: '100%',
+  },
+  textBlockInner: {
+    paddingTop: '9px',
+    msoLineHeightRule: 'exactly',
+    msTextSizeAdjust: '100%',
+    WebkitTextSizeAdjust: '100%',
+  },
+  textContentContainer: {
+    maxWidth: '100%',
+    minWidth: '100%',
+    borderCollapse: 'collapse',
+    msoTableLspace: '0pt',
+    msoTableRspace: '0pt',
+    msTextSizeAdjust: '100%',
+    WebkitTextSizeAdjust: '100%',
+  },
+  textContent: {
+    padding: '0px 18px 9px',
+    lineHeight: '150%',
+    msoLineHeightRule: 'exactly',
+    msTextSizeAdjust: '100%',
+    WebkitTextSizeAdjust: '100%',
+    wordBreak: 'break-word',
+    color: '#111111',
+    fontFamily: '"Source Sans Pro","Helvetica Neue", Helvetica, Arial,sans-serif',
+    fontSize: '18px',
+    textAlign: 'left',
+  },
+};
