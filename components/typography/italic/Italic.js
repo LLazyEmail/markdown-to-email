67b23845688d37f37f51bc03c45832fa250c5934
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-13 13:25:30 +0000

    clean up

--- components/typography/italic/Italic.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Italic = ({ children }) => (
-  <i>
-    {children}
-  </i>
-);
-
-export default Italic;
--- components/typography/link/styles.js
+++
@@ -1,5 +0,0 @@
-export const styles = {
-  link: {
-    msoLineHeightRule: 'exactly', msTextSizeAdjust: '100%', WebkitTextSizeAdjust: '100%', color: '#111111', fontWeight: 'bold', textDecoration: 'underline',
-  },
-};
