49db9ea6614223fdb31e83a9e5d38d984dde24d2
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-13 10:40:41 +0000

    updates

---
+++ package/hn/body/src/index.js
@@ -0,0 +1,18 @@
+/* eslint import/extensions: 0 */
+
+// import Divider from './divider/Divider.jsx';
+// import Heading from './heading/Heading.jsx';
+// import Image from './image/Image.jsx';
+// import Italic from './italic/Italic.jsx';
+// import Link from './link/Link.jsx';
+// import List from './list/List.jsx';
+// import mainTitle from './mainTitle/mainTitle.jsx';
+// import Paragraph from './paragraph/Paragraph.jsx';
+// import Separator from './separator/Separator.jsx';
+// import Strong from './strong/Strong.jsx';
+// import Subtitle from './subtitle/Subtitle.jsx';
+
+// export {
+//     Divider, Heading, Image, Italic, Link, List, mainTitle, 
+//     Paragraph, Separator, Strong, Subtitle
+// };
---
+++ templates/content-only/readme.md
@@ -0,0 +1,8 @@
+
+Divider, Heading, Image, Italic, Link, List, mainTitle, 
+Paragraph, Separator, Strong, Subtitle
+
+----
+
+
+Address, Copyright, headStyles, NewsletterSponsorshipLink, Unsubscribe
