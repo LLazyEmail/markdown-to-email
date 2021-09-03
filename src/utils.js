604718cd43744911bd81477c850edc480be3af18
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-03 11:53:52 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,15 +2,6 @@ const fs = require('fs');
 const chalk = require('chalk');
 const { forEach } = require('lodash');
 
-const MESSAGE_HTML_CONTENT_ONLY = 'The content has been parsed successfully';
-const MESSAGE_HTML_FULL_TEMPLATE = 'The full-template has been parsed successfully';
-const MESSAGE_HTML_FULL_TEMPLATE2 = 'The full-template has been parsed successfully2';
-
-const MESSAGE_REACT_FULL_TEMPLATE = 'The FullTemplate has been parsed successfully';
-const MESSAGE_REACT_CONTENT = 'The Content has been parsed successfully';
-
-
-
 const reactComponent = `
 import React from "react";
 
@@ -87,11 +78,5 @@ module.exports = {
   
   FULL_SOURCE,
   CONTENT_SOURCE,
-  
-MESSAGE_HTML_CONTENT_ONLY,
-MESSAGE_HTML_FULL_TEMPLATE,
-MESSAGE_HTML_FULL_TEMPLATE2,
-
-MESSAGE_REACT_FULL_TEMPLATE,
-MESSAGE_REACT_CONTENT
+  generateTemplateName
 };
