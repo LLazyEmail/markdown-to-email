b41fc449e090f7ceaf5e4b750f99111978df0a5a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-20 05:36:12 +0300

    Update callbacks.js

--- package/html/callbacks/src/plain-version/callbacks.js
+++ package/html/callbacks/src/plain-version/callbacks.js
@@ -4,9 +4,10 @@ import { replaceHTMLWrapper, newLine } from '../helpers';
 
 function paragraphWrapper(text, line) {
   const trimmed = line.trim();
-
+  
+  // TODO move out this regex into constants file.
   if (/^<\/?(ul|ol|li|h|p|bl)/i.test(trimmed)) {
-    // @TODO move out this regex into constants file.
+    
     return newLine + line + newLine;
   }
 
@@ -35,19 +36,22 @@ function image(text, alt, srcWithTooltip) {
 }
 
 function br(text, newLines) {
+  // TODO move out this regex into constants file.
   const arrNewLines = newLines.match(new RegExp(newLine, 'g'));
 
   // @TODO well, it's not good. can be improved with lodash
+    
   const result = arrNewLines.reduce((acc, current, index) => (index > 0 ? `${acc}<br/>${current}` : current), '');
 
   return result;
 }
 
 function sponsorship(text) {
+  // TODO move out this regex into constants file.
   const regex = /\[(.*?)\]/g;
   const [content, href, src] = text
     .match(regex)
-    .map((match) => match.replace(/[\[\]]/g, ''));
+    .map((match) => match.replace(/[\[\]]/g, ''));   // TODO move out this regex into constants file.
 
   const config = {
     src: src.trim(),
