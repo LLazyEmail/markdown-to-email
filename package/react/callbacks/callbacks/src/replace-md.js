4b37fb46fe3f60913a31eaf1af2545af09c102c3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-21 21:54:32 +0300

    Update replace-md.js

--- package/react/callbacks/callbacks/src/replace-md.js
+++ package/react/callbacks/callbacks/src/replace-md.js
@@ -1,3 +1,6 @@
+import { REGEXP_PREVIEW_TEXT } from 'atherdon-newsletter-constants';
+
+
 // @TODO explore it later
 // try {
 //   myroutine(); // may throw three types of exceptions
@@ -14,22 +17,22 @@
 //     /* work in case there is an error */
 //     throw e;
 //   }
+
 // https://gist.github.com/harrylove/1230566/d064e5c216384d3846f73ed555e9899be02e8f98#gistcomment-2884621
 // https://stackoverflow.com/a/326693
 function replaceMarkdown(regexp, callback) {
     
-    // console.log('helpers- replace markdown method')
-    // console.log(typeof callback)  
+  // console.log(typeof callback)  
 
   var fixedCallbackMethod = false;
+ 
   switch (typeof callback) {
       case 'string':
           fixedCallbackMethod = callback;
           break;
 
       case 'undefined':
-            console.log("ERRROROROR HERE!!!!")
-
+          console.log("ERRROROROR HERE!!!!")
           break;  
 
       default:
@@ -42,27 +45,18 @@ function replaceMarkdown(regexp, callback) {
 
 // i think this method would be broken now, because we cant play with "this."
 // @TODO find out if we really using this method or not?
-function replaceMarkdownPreviewText(regexp) {
+function replaceMarkdownPreviewText() {
   const config = {
     content: this.content.trim(),
   };
 
-  /*
-  this.previewText = replaceHTMLWrapper(
-        "previewText",
-        config,
-        "body"
-      );
-      console.log(this.previewText)
- */
   this.errors.previewText = true;
 
   this.content = this.content.replace(
-        regexp, 
+        REGEXP_PREVIEW_TEXT, 
         () => this.previewText
     );
 }
-
 
 export {
   replaceMarkdown,
