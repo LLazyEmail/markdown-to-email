92126df20827584ec16b4b06e75f5d473d048958
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-27 13:15:24 +0000

    updates and re-organization

--- package/GENERATOR/src/utils.js
+++ package/GENERATOR/src/utils.js
@@ -6,19 +6,32 @@ import write from 'write';
 const CONST_FILE_CREATED = '';
 const CONST_FILE_NOT_WRITTEN = '';
 
+const ERROR_NO_TITLE = '`title` is a required option for `renderTemplate`';
+const ERROR_NO_BODY_CONTENT = '`bodyContent` is a required option for `renderTemplate`';
+const ERROR_NO_PREVIEW_TEXT = '`previewText` is a required option for `renderTemplate`';
+
+const WARNING_EMAIL_LENGTH = `Email output is ${Math.round(bytes / 1024)}KB. ` +
+'It is recommended to keep the delivered HTML to smaller ' +
+'than 100KB, to avoid getting emails cut off or rejected due to spam.';
+
+
+
+
 function ____write(fileName, content, dir = "generated", message) {
   const _path = `${dir}/${fileName}`; // @todo it's not an ideal thing
 
   // promise
   write(_path, content)
     .then(() => {
-      // do stuff
+      
+      // i dont like this line @TODO change it
+      message && console.log(`file has been written successfully${fileName}`);
+    })
+    .catch(err => { 
+      // console.log(err) 
+      throw new Error('file not written');
     });
 
-  // callback
-  write(_path, content, (err) => {
-    // do stuff with err
-  });
 }
 
 // writeHTML
@@ -47,15 +60,12 @@ function isFolderExists(dir) {
 
 const generateTemplateName = (suffix, ext = 'html') => `${suffix}-${Date.now()}.${ext}`;
 
+//--------
 const countingBytes = (html) => {
   const bytes = Buffer.byteLength(html, 'utf8');
 
   if (bytes > 1024 * 100) {
-    console.warn(
-      `Email output is ${Math.round(bytes / 1024)}KB. ` +
-        'It is recommended to keep the delivered HTML to smaller ' +
-        'than 100KB, to avoid getting emails cut off or rejected due to spam.'
-    );
+    console.warn( WARNING_EMAIL_LENGTH );
   }
 
   return bytes;
@@ -73,10 +83,16 @@ const countingBytes = (html) => {
 //   throw new Error('`previewText` is a required option for `renderTemplate`');
 // }
 
+//-----------
+
 export {
   __write,
   readSourceFile,
   isFolderExists,
   generateTemplateName,
-  countingBytes
+  countingBytes,
+
+  ERROR_NO_TITLE,
+  ERROR_NO_BODY_CONTENT,
+  ERROR_NO_PREVIEW_TEXT,
 };
