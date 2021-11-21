79fcc3ebcbb8b9030106d6032e93acb20770e452
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-11-21 17:58:43 +0200

    Delete address.js

--- package/html/components/Template/miscellaneous/src/partials/address.js
+++
@@ -1,7 +0,0 @@
-// import { mailingAddress } from '../config';
-import { config } from '../config';
-
-export const address =
-`<strong>Our mailing address is:</strong>
-<br>
-${config.mailingAddress}`;
