8a9b2da487ca2e69088606e4693c7394abea90ce
Vadim <35640573+vadim9999@users.noreply.github.com>
2022-05-29 13:02:08 +0300

    change  back

--- checkFolders.js
+++ checkFolders.js
@@ -3,9 +3,3 @@ const { isFolderExists } = require('./src/utils');
 isFolderExists('generated');
 isFolderExists('./src/tests/_generated');
 isFolderExists('./src/parserMDReact/tests/_generated');
-
-function isFolderExists(dir) {
-  if (!existsSync(dir)) {
-    mkdirSync(dir);
-  }
-}
