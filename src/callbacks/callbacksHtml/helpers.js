5ae0ee61e629130cc980a66142d6ce2abd4eafdc
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-16 13:32:56 +0300

    Update helpers.js

--- src/callbacks/callbacksHtml/helpers.js
+++ src/callbacks/callbacksHtml/helpers.js
@@ -12,6 +12,10 @@ const newLine = '\n';
 
 // eslint-disable-next-line no-unused-vars
 function replaceWrapper(name, config, folder = 'typography') {
+
+
+
+  
   // TODO later we can get rid of it completely.
   // right now it's only for back campatibility purposes
   // const configCopy = Object.assign(config, {
@@ -21,6 +25,8 @@ function replaceWrapper(name, config, folder = 'typography') {
   // });
   // console.log(configCopy);
   // return generateNewString(name, configCopy);
+
+
 }
 
 export {
