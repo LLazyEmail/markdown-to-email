b84dd8502a831b484de9879d0cc04ef5cf5b3174
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-21 21:51:40 +0000

    reogranization

--- package/html/callbacks/src/methods/simple.js
+++ package/html/callbacks/src/methods/simple.js
@@ -1,26 +1,6 @@
 import { replaceWrapper, newLine } from '../helpers';
 
-const italic = () => '$1<em>$3</em>$4';
 
-// const del = '<del>$1</del>';
-
-const del = () => '<del>$1</del>';
-
-// const q = '<q>$1</q>';
-
-const q = () => '<q>$1</q>';
-
-// const code = '<code>$1</code>';
-
-const code = () => '<code>$1</code>';
-
-// const hr = `${newLine}<hr />`;
-
-const hr = () => `${newLine}<hr />`;
-
-// const empty = '';
-
-const empty = () => '';
 
 
 function strong(text, doubleAsterix, content, asterix) {
@@ -128,11 +108,5 @@ export {
   blockquote,
   mem,
   header,
-  italic,
-  del,
-  q,
-  code,
-  hr,
-  empty,
   previewText
 };
