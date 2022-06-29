dfb4040d36bd529771c1f9fa9a649e9acb0f3218
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-29 17:23:36 +0000

    123

--- src/domain/plainjs-template/template/index.js
+++ src/domain/plainjs-template/template/index.js
@@ -1,1 +1,19 @@
 //-----------------------
+
+import {
+    printMain,
+    printFooter,
+    printBody
+} from 'atherdon-old-newsletter-js-outertemplate';
+
+const plainV2 = () => {
+
+
+    console.log(printMain());
+    console.log(printFooter());
+    console.log(printBody());
+
+
+}
+
+export default plainV2;
