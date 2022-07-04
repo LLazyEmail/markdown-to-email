b9b4b2666c00ed638057b66e12db246c22f67ccd
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-04 00:20:44 +0000

    cleaning utils

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -15,10 +15,6 @@ const ERROR_NO_BODY_CONTENT =
 const ERROR_NO_PREVIEW_TEXT =
   '`previewText` is a required option for `renderTemplate`';
 
-// const WARNING_EMAIL_LENGTH = `Email output is ${Math.round(bytes / 1024)}KB. ` +
-// 'It is recommended to keep the delivered HTML to smaller ' +
-// 'than 100KB, to avoid getting emails cut off or rejected due to spam.';
-
 // ---
 
 // eslint-disable-next-line default-param-last
@@ -66,34 +62,10 @@ function readSourceFile(fileName) {
   return readFileSync(fileName, { encoding: 'utf-8' });
 }
 
-// https://www.npmjs.com/package/directory-exists
-// https://www.npmjs.com/package/path-exists this is better
 
-function isFolderExists(dir) {
-  if (!existsSync(dir)) {
-    mkdirSync(dir);
-  }
-}
-
-// can be renamed into a generateFileName
-// https://befused.com/javascript/get-filename-url/
-const generateTemplateName = (suffix, ext = 'html') =>
-  `${suffix}-${Date.now()}.${ext}`;
-
-//--------
-// const countingBytes = (html) => {
-//   const bytes = Buffer.byteLength(html, 'utf8');
-
-//   if (bytes > 1024 * 100) {
-//     console.warn( WARNING_EMAIL_LENGTH );
-//   }
-
-//   return bytes;
-// };
-
-//-----------
 //------ This file used for back capability purposes
 // moving things into 'domain' folder.
+
 import {
   writeHTML,
   __write,
