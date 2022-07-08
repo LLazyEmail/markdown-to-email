7e94f0debd29819388b3c6229a2c5ea45f340f20
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-08 13:49:49 +0000

    update

--- src/domain/replacer-class/configuration-react.js
+++ src/domain/replacer-class/configuration-react.js
@@ -6,10 +6,40 @@
     
 //   } from '../replace-markdown/replace-md';
 
-//  import Replacer from '../../parserMDHtml/replace';
+import Replacer from '../../parserMDHtml/replace';
 
-function configureReplacer(state) {
-    
-}
+ function configureReplacer(state, isFull) {
+    // CAN WE MOVE OUT CALLBACKS INTO REPLACER?????
+    ReactReplacer.replaceMDBinded = replaceMarkdown.bind(state);
+  
+    // I think we didnt use it quite often
+    // ReactReplacer.replaceMDBindedPreviewText = replaceMarkdownPreviewText.bind(state);
+  
+    // NOTE DON'T CHANGE ORDER OF FUNCTION CALLS
+    if (isFull) {
+      ReactReplacer.previewText();
+    }
+  
+    ReactReplacer.comments();
+    ReactReplacer.typography();
+  
+    ReactReplacer.ulList();
+    // TODO UPDATE OLLIST CALLBACK, IT'S NOT WORKING
+    //   ReactReplacer.olList()
+  
+    ReactReplacer.blockQuote();
+    ReactReplacer.hr();
+    ReactReplacer.paragraph();
+  
+    // TODO CHECK CASES WHEN CONTENT CAN HAVE EMPTY LISTS OR BLOCKQUOTE
+    //   ReactReplacer.emptyUl();
+    //   ReactReplacer.emptyOl();
+    //   ReactReplacer.emptyBlockQuote();
+  
+    ReactReplacer.br();
+    ReactReplacer.sponsorship();
+    ReactReplacer.mem();
+    ReactReplacer.separator();
+  }
 
 export default configureReplacer;
