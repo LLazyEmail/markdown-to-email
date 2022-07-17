872ca67eae9c28cc8b925e72bab5f8361358c527
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-17 21:53:52 +0000

    updates in literals

--- src/domain/email-prototypes/react/typography/index.js
+++ src/domain/email-prototypes/react/typography/index.js
@@ -1,25 +1,35 @@
-// import headingLiteral from './literals/heading';
-// import imageLiteral from './literals/image';
-// import italicLiteral from './literals/italic';
-// import linkLiteral from './literals/link';
-// import listLiteral from './literals/list';
-// import listItemLiteral from './literals/listItem';
-// import titleLiteral from './literals/mainTitle';
-// import paragraphLiteral from './literals/paragraph';
-// import strongLiteral from './literals/strong';
-// import subtitleLiteral from './literals/subtitle';
-// import separatorLiteral from './literals/separator';
+import headingLiteral from './literals/heading';
+import imageLiteral from './literals/image';
+import italicLiteral from './literals/italic';
+import linkLiteral from './literals/link';
+import listLiteral from './literals/list';
+import listItemLiteral from './literals/listItem';
+import titleLiteral from './literals/mainTitle';
+import paragraphLiteral from './literals/paragraph';
+import qLiteral from './literals/q';
+import strongLiteral from './literals/strong';
+import subtitleLiteral from './literals/subtitle';
+import separatorLiteral from './literals/separator';
 
-// export {
-//   headingLiteral,
-//   imageLiteral,
-//   italicLiteral,
-//   linkLiteral,
-//   listLiteral,
-//   listItemLiteral,
-//   titleLiteral,
-//   paragraphLiteral,
-//   strongLiteral,
-//   subtitleLiteral,
-//   separatorLiteral,
-// };
+import blockquoteLiteral from './literals/blockquote';
+import codeLiteral from './literals/code';
+import delLiteral from './literals/del';
+
+export {
+  headingLiteral,
+  imageLiteral,
+  italicLiteral,
+  linkLiteral,
+  listLiteral,
+  listItemLiteral,
+  titleLiteral,
+  paragraphLiteral,
+  qLiteral,
+  strongLiteral,
+  subtitleLiteral,
+  separatorLiteral,
+
+  blockquoteLiteral,
+  codeLiteral,
+  delLiteral
+};
