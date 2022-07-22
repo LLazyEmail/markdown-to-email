1a86898f44ee5ec749e3cb7ee27a8d9bf9a03a39
Vadim <vadim.putrov@gmail.com>
2022-07-22 22:27:44 +0300

    clean

--- src/templates/PlainJSOuterTemplate/index.js
+++ src/templates/PlainJSOuterTemplate/index.js
@@ -1,27 +1,3 @@
 import { generateFullTemplateHackernoon } from './generateFullTemplate';
 
-export {
-  //   body,
-
-  //   //spread misc
-  //   address,
-  //   copyrights,
-  //   fonts,
-  //   headStyles,
-  //   newsletterSponsorshipLink,
-  //   unsubscribe,
-  //   head,
-
-  // fullTemplateOLD,
-  // headline,
-  // logoBottom,
-  // logoTop,
-  // previewText,
-  // section,
-  // sponsor,
-  // ctaList,
-  generateFullTemplateHackernoon,
-  // printMain,
-  // printFooter,
-  // printBody,
-};
+export { generateFullTemplateHackernoon };
