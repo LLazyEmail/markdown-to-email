73e4272dc2d87f03f63184f08ec484666e1e5ed2
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-22 09:49:19 +0000

    update

--- src/domain/email-prototypes/plainjs/template/index.js
+++ src/domain/email-prototypes/plainjs/template/index.js
@@ -10,6 +10,8 @@ const plainV2 = () => {
   console.log(TObject.printMain());
   console.log(TObject.printFooter());
   console.log(TObject.printBody());
+  // ----------------
+  console.log(TObject.printTemplate('{content}'));
 };
 
 export default plainV2;
