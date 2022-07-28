54d7ca50b4bab1a7e3e2565db8197f1cc19c4fb5
Vadim <vadim.putrov@gmail.com>
2022-07-28 18:48:24 +0300

    refactor map

--- src/domain/react/configuration/index.js
+++ src/domain/react/configuration/index.js
@@ -1,33 +1,5 @@
-import { replaceMarkdownReact, Replacer } from '../index';
+import Replacer from '../Replacer.class';
 
-function configureReplacer(state, isFull) {
-  Replacer.replaceMDBinded = replaceMarkdownReact.bind(state);
-
-  // NOTE DON'T CHANGE ORDER OF FUNCTION CALLS
-  if (isFull) {
-    Replacer.previewText();
-  }
-
-  Replacer.comments();
-  Replacer.typography();
-
-  Replacer.ulList();
-  // TODO UPDATE OLLIST CALLBACK, IT'S NOT WORKING
-  //   Replacer.olList()
-
-  // Replacer.blockquote();
-  Replacer.hr();
-  Replacer.paragraph();
-
-  // TODO CHECK CASES WHEN CONTENT CAN HAVE EMPTY LISTS OR BLOCKQUOTE
-  //   Replacer.emptyUl();
-  //   Replacer.emptyOl();
-  //   Replacer.emptyBlockquote();
-
-  Replacer.br();
-  Replacer.sponsorship();
-  // Replacer.mem();
-  // Replacer.separator();
-}
+const configureReplacer = (state) => Replacer.configure(state);
 
 export default configureReplacer;
