195f4a2701c746a84b1f84bec39cb3aee66851ac
Vadim <vadim.putrov@gmail.com>
2022-07-29 18:11:08 +0300

    fix lint errors lists.js

--- src/callbacksReact/methods/lists.js
+++ src/callbacksReact/methods/lists.js
@@ -1,85 +1,82 @@
-/* eslint-disable */
 import { newLine } from '../../domain/helper-methods';
 
-const _hr = () => `${newLine}<hr />`;
-const _empty = () => '';
+// const _hr = () => `${newLine}<hr />`;
+// const _empty = () => '';
 
+// import { commonReplaceReact } from '../../domain/replace-wrapper3.0';
 
-import {commonReplaceReact} from '../../domain/replace-wrapper3.0'
+// function getParsedSubList(subList) {
+//   return subList.replace(
+//     new RegExp(`\\s{4}\\*(.*?)${newLine}`, 'g'),
+//     (text, subItem) => {
+//       const params = {
+//         content: subItem.trim(),
+//       };
 
+//       const config = {
+//         params,
+//         name: 'listItem',
+//         debug: true,
+//       };
 
-function getParsedSubList(subList) {
-  return subList.replace(
-    new RegExp(`\\s{4}\\*(.*?)${newLine}`, 'g'),
-    (text, subItem) => {
-      const params = {
-        content: subItem.trim(),
-      };
+//       const replaced = commonReplaceReact(config);
 
-      const config = {
-        params,
-        name: 'listItem',
-        debug: true,
-      };
-
-      const replaced = commonReplaceReact(config);
-
-      return newLine + replaced;
-
-          
-    },
-  );
-}
+//       return newLine + replaced;
+//     },
+//   );
+// }
 
 // @TODO it looks even more crazier than it was 2 months ago
 // i'm not suprised that it might get errors(but works fine now)
-function _ulList(text, list) {
+function _ulList() {
+  // text,
+  // list
   // @todo improve this crazy structure.
-  const parsedSubListsParts = list.replace(
-    new RegExp(`((\\s{4}\\*(.*?)${newLine}){1,})`, 'g'),
-    (text, subList) => {
-      const parsedSubItem = getParsedSubList(subList);
+  // const parsedSubListsParts = list.replace(
+  //   new RegExp(`((\\s{4}\\*(.*?)${newLine}){1,})`, 'g'),
+  //   () =>
+  //     // text,
+  //     // subList
+  //     {
+  //       // const parsedSubItem = getParsedSubList(subList);
 
-      // const params = {
-      //   content: parsedSubItem + newLine,
-      //   type: 'UL',
-      // };
+  //       // const params = {
+  //       //   content: parsedSubItem + newLine,
+  //       //   type: 'UL',
+  //       // };
 
-      // const config = {
-      //   params,
-      //   name: 'list',
-      //   debug: true,
-      // };
+  //       // const config = {
+  //       //   params,
+  //       //   name: 'list',
+  //       //   debug: true,
+  //       // };
 
-      const replaced = commonReplaceReact(config);
+  //       // const replaced = commonReplaceReact(config);
 
-      return newLine + replaced;
-    
-    },
-  );
+  //       // return newLine + replaced;
+  //       return 'this is UL list callback needs to revise it';
+  //     },
+  // );
 
-  const parsedList = parsedSubListsParts.replace(
-    new RegExp(`\\*(.*?)${newLine}`, 'g'),
-    (text, listItem) => {
-      // const params = {
-      //   content: listItem.trim(),
-      // };
+  // const parsedList = parsedSubListsParts.replace(
+  //   new RegExp(`\\*(.*?)${newLine}`, 'g'),
+  //   (text, listItem) => {
+  //     // const params = {
+  //     //   content: listItem.trim(),
+  //     // };
 
-      // const config = {
-      //   params,
-      //   name: 'listItem?',
-      //   debug: true,
-      // };
+  //     // const config = {
+  //     //   params,
+  //     //   name: 'listItem?',
+  //     //   debug: true,
+  //     // };
 
-        const replaced = commonReplaceReact(config);
+  //     const replaced = commonReplaceReact(config);
 
-        return newLine + replaced;
-
-      
-
-      
-    },
-  );
+  //     return newLine + replaced;
+  //   },
+  return 'this is UL list callback needs to revise it';
+  // );
 
   // const params = {
   //   content: parsedList + newLine,
