466e8c54113d03b8124e9291738ab469bdba53a8
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-02-28 17:55:43 +0000

    prettier - tags

--- src/constants/addon.js
+++ src/constants/addon.js
@@ -9,8 +9,6 @@ const REGEXP_SECTION_TAGS = /^<\/?(ul|ol|li|h|p|bl)/i;
 const REGEXP_IMAGE_TOOLTIP = /\"image_tooltip\"/;
 const REGEXP_NEWLINE = `${newLine}g`;
 
-// const REGEXP_SPONSORHSHIP =
-
 // command-line-methods.js
 // const REGEXP_BASIC_TABLE_TAGS = new RegExp('(<table|<tr>|<td|<tbody>)', 'i');
 
