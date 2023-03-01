e13da614294831c663b1e4434bf3c53294996ad7
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-03-01 17:23:36 +0000

    not working yet

--- src/domain/write/index.js
+++ src/domain/write/index.js
@@ -1,6 +1,13 @@
-import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
+import { writeFileSync } from 'fs';
 
-import write from 'write';
+import {
+  writeHTML,
+  readSourceFile,
+  isFolderExists,
+  generateTemplateName,
+} from 'markup-generator';
+
+// import write from 'write';
 
 import {
   CONST_FILE_NOT_WRITTEN,
@@ -10,23 +17,23 @@ import {
 } from './constant-messages';
 
 // eslint-disable-next-line default-param-last
-function writeHTML(fileName, content, dir = 'generated', message) {
-  // @todo it's not an ideal thing
-  // eslint-disable-next-line no-underscore-dangle
-  const _path = `${dir}/${fileName}`;
+// function writeHTML(fileName, content, dir = 'generated', message) {
+//   // @todo it's not an ideal thing
+//   // eslint-disable-next-line no-underscore-dangle
+//   const _path = `${dir}/${fileName}`;
 
-  // promise
-  write(_path, content)
-    .then(() => {
-      // i dont like this line @TODO change it
-      // eslint-disable-next-line no-unused-expressions
-      message && console.log(`file has been written successfully${fileName}`);
-    })
-    .catch(() => {
-      // console.log(err)
-      throw new Error(CONST_FILE_NOT_WRITTEN);
-    });
-}
+//   // promise
+//   write(_path, content)
+//     .then(() => {
+//       // i dont like this line @TODO change it
+//       // eslint-disable-next-line no-unused-expressions
+//       message && console.log(`file has been written successfully${fileName}`);
+//     })
+//     .catch(() => {
+//       // console.log(err)
+//       throw new Error(CONST_FILE_NOT_WRITTEN);
+//     });
+// }
 
 // TODO I think we can delete this method
 // __write - an old version of a method that we have
@@ -52,23 +59,23 @@ function __write(
   message && console.log(`file has been written successfully${fileName}`);
 }
 
-function readSourceFile(fileName) {
-  return readFileSync(fileName, { encoding: 'utf-8' });
-}
+// function readSourceFile(fileName) {
+//   return readFileSync(fileName, { encoding: 'utf-8' });
+// }
 
 // https://www.npmjs.com/package/directory-exists
 // https://www.npmjs.com/package/path-exists this is better
 
-function isFolderExists(dir) {
-  if (!existsSync(dir)) {
-    mkdirSync(dir);
-  }
-}
+// function isFolderExists(dir) {
+//   if (!existsSync(dir)) {
+//     mkdirSync(dir);
+//   }
+// }
 
 // can be renamed into a generateFileName
 // https://befused.com/javascript/get-filename-url/
-const generateTemplateName = (suffix, ext = 'html') =>
-  `${suffix}-${Date.now()}.${ext}`;
+// const generateTemplateName = (suffix, ext = 'html') =>
+//   `${suffix}-${Date.now()}.${ext}`;
 
 //--------
 // const countingBytes = (html) => {
@@ -80,8 +87,6 @@ const generateTemplateName = (suffix, ext = 'html') =>
 
 //   return bytes;
 // };
-
-//-----------
 
 export {
   writeHTML,
