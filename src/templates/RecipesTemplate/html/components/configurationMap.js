490d9625ec9d5c60796e8ad97ff72308dc15c129
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-03-12 17:54:10 +0000

    upd

--- src/templates/RecipesTemplate/html/components/configurationMap.js
+++ src/templates/RecipesTemplate/html/components/configurationMap.js
@@ -1,27 +1,26 @@
-import {
-  heading,
-  renderSubHeading,
-  // spanHeadingBlock,  // will pick it back, disabled for husky
-  separatorComponent,
-} from 'nmtg-template-mailerlite-typography';
+import // heading,
+// renderSubHeading,
+// spanHeadingBlock,  // will pick it back, disabled for husky
+// separatorComponent,
+'nmtg-template-mailerlite-typography';
 import {
   // buttonComponent,   // will pick it back, disabled for husky
   // divComponent,      // will pick it back, disabled for husky
-  imageComponent,
+  // imageComponent,
   // imageLink,         // will pick it back, disabled for husky
   // italicComponent,   // will pick it back, disabled for husky
-  linkComponent,
+  // linkComponent,
   // listComponent,     // will pick it back, disabled for husky
   // listItemComponent, // will pick it back, disabled for husky
-  paragraphComponent,
+  // paragraphComponent,
   strongComponent,
 } from 'html-typography-tags';
 
 import {
   _strong,
-  _link,
+  // _link,
   _blockquote,
-  _header,
+  // _header,
   //--------
   // _italic,
   // _italicAsterix,
@@ -36,16 +35,16 @@ import {
   _olList,
   // getParsedSubList,
   //--------------
-  _image,
-  _paragraphWrapper,
+  // _image,
+  // _paragraphWrapper,
   _br,
-  _separator,
+  // _separator,
 } from '../../../../callbacks/html/index';
 
 import {
-  REGEXP_HEADER,
-  REGEXP_IMAGE,
-  REGEXP_LINK,
+  // REGEXP_HEADER,
+  // REGEXP_IMAGE,
+  // REGEXP_LINK,
   REGEXP_STRONG,
   REGEXP_DEL,
   REGEXP_Q,
@@ -56,8 +55,8 @@ import {
   REGEXP_HR,
   REGEXP_BR,
   // REGEXP_EM,
-  REGEXP_PARAGRAPH,
-  REGEXP_SEPARATOR,
+  // REGEXP_PARAGRAPH,
+  // REGEXP_SEPARATOR,
 } from '../../../../constants/index';
 
 import objectBuilder from '../../../../domain/objectBuilder';
@@ -66,17 +65,19 @@ import objectBuilder from '../../../../domain/objectBuilder';
 
 const map = {
   strong: objectBuilder(REGEXP_STRONG, _strong, strongComponent),
-  link: objectBuilder(REGEXP_LINK, _link, linkComponent),
+  // link: objectBuilder(REGEXP_LINK, _link, linkComponent),
   del: objectBuilder(REGEXP_DEL, _del, false),
-  image: objectBuilder(REGEXP_IMAGE, _image, imageComponent),
+  // image: objectBuilder(REGEXP_IMAGE, _image, imageComponent),
+  // -----
   // italic: objectBuilder(REGEXP_UNDERSCORE_ITALIC, _italic, italicComponent),
   // italic_asterix: objectBuilder(
   //   REGEXP_ASTERIX_ITALIC,
   //   _italic,
   //   italicComponent,
   // ),
-  header: objectBuilder(REGEXP_HEADER, _header, heading),
-  subtitle: objectBuilder(REGEXP_HEADER, _header, renderSubHeading),
+  // -----
+  // header: objectBuilder(REGEXP_HEADER, _header, heading),
+  // subtitle: objectBuilder(REGEXP_HEADER, _header, renderSubHeading),
   // title: objectBuilder(REGEXP_HEADER, _header, titleComponent),
   q: objectBuilder(REGEXP_Q, _q, false),
   code: objectBuilder(REGEXP_CODE, _code, false),
@@ -90,13 +91,15 @@ const map = {
   olList: objectBuilder(REGEXP_OL_LIST, _olList, false),
   blockquote: objectBuilder(REGEXP_BLOCKQUOTE, _blockquote, false),
   hr: objectBuilder(REGEXP_HR, _hr, false),
-  paragraph: objectBuilder(
-    REGEXP_PARAGRAPH,
-    _paragraphWrapper,
-    paragraphComponent,
-  ),
+  // paragraph: objectBuilder(
+  //   REGEXP_PARAGRAPH,
+  //   _paragraphWrapper,
+  //   paragraphComponent,
+  // ),
   br: objectBuilder(REGEXP_BR, _br),
-  separator: objectBuilder(REGEXP_SEPARATOR, _separator, separatorComponent),
+  // separator: objectBuilder(REGEXP_SEPARATOR, _separator, separatorComponent),
 };
+
+// const map = {};
 
 export default map;
